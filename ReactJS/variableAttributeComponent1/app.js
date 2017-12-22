fe19c9c8413a7162d6c37ef849c5b381dbd0046a
ChristineI <kristeen.cal@gmail.com>
2017-12-21 23:57:49 -0800

    render component and get picture properties

---
+++ ReactJS/variableAttributeComponent1/app.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const redPanda = {
+  src: 'http://bit.ly/1U92LL3',
+  alt: 'Red Panda',
+  width:  '200px'
+};
+
+class RedPanda extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Cute Red Panda</h1>
+        <img
+          src={redPanda.src}
+          alt={redPanda.alt}
+          width={redPanda.width} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <RedPanda />,
+  document.getElementById('app')
+);
