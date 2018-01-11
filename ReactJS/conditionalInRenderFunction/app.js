f3a84cbd4a54072be9270a253746e238243ffcb2
ChristineI <kristeen.cal@gmail.com>
2018-01-10 23:43:19 -0800

    still working

--- ReactJS/conditionalInRenderFunction/app.js
+++ ReactJS/conditionalInRenderFunction/app.js
@@ -5,13 +5,14 @@ const fiftyFifty = Math.random() < 0.5;
 
 // New component class starts here:
 class TonightsPlan extends React.Component {
+
   render () {
     let task;
     if (fiftyFifty = true) {
-      task = "Tonight I'm going out WOOO"
+      task = 'Tonight Im going out WOOO'
     }
     else {
-      task = "Tonight I'm going to bed WOOO"
+      task = 'Tonight Im going to bed WOOO'
     }
 
     return <h1>{task}</h1>;
