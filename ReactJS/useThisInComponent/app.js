c6945d6c20192b5908b85f86ceb8bf6431dc9b7f
ChristineI <kristeen.cal@gmail.com>
2018-01-18 16:34:33 -0800

    adding a getter method to the class body

---
+++ ReactJS/useThisInComponent/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyName extends React.Component {
+	// name property goes here:
+	get name () {
+    return string;
+  }
+
+  render() {
+    return <h1></h1>;
+  }
+}
+
+ReactDOM.render(<MyName />, document.getElementById('app'));
