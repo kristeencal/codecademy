3d0ae625d8fb5c229e9fc14589df2b9962c4f7ce
ChristineI <kristeen.cal@gmail.com>
2018-01-24 17:50:24 -0800

    adding an event listener to a component by including it in the render function

---
+++ ReactJS/eventListenerInComponent/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Button extends React.Component {
+  scream() {
+    alert('AAAAAAAAHHH!!!!!');
+  }
+
+  render() {
+    return <button onClick={this.scream}>AAAAAH!</button>;
+  }
+}
